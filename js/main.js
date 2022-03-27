c10aa823040a9a753359b33d63604defb99651e5
Gothalion <mbarsukovv@gmail.com>
2022-03-27 23:38:27 +0300

    Added: values for variables

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-let title;
-let screens;
-let screenPrice;
-let rollback;
-let fullPrice;
-let adaptive;
+let title = 'Калькулятор верстки';
+let screens = 'Простые, сложные, интерактивные';
+let screenPrice = 2300;
+let rollback = 11;
+let fullPrice = 7500;
+let adaptive = true;
 
 alert('Hello, world!');
 console.log('I want to learn JavaScript');
