a3640d19f135cb4f0c7422810ed1ba60d2b56650
Gothalion <mbarsukovv@gmail.com>
2022-03-27 23:47:01 +0300

    Added: console logs

--- js/main.js
+++ js/main.js
@@ -5,5 +5,20 @@ let rollback = 11;
 let fullPrice = 7500;
 let adaptive = true;
 
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+console.log(screens.length);
+console.log(
+  'Стоимость верстки экранов ' + screenPrice + ' рублей/долларов/гривен/юани'
+);
+console.log(
+  'Стоимость разработки сайта ' + fullPrice + ' рублей/долларов/гривен/юани'
+);
+console.log(screens.toLowerCase());
+console.log(screens.split());
+
+console.log(fullPrice * (rollback / 100));
+
 alert('Hello, world!');
 console.log('I want to learn JavaScript');
